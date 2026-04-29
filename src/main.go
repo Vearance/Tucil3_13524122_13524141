7fcf2df4a1d68d3cf1f32570153c21408234b36b
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-04-29 19:47:34 +0700

    refactor: main

--- src/main.go
+++ src/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	var filename string
+	fmt.Print("Masukan file input : ")
+	fmt.Scanln(&filename)
+
+	board, startPos, err := LoadMap(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	start := time.Now()
+	// Solve()
+	elapsed := time.Since(start)
+
+	fmt.Printf("Waktu eksekusi: %d ms\n", elapsed.Milliseconds()) [cite: 89, 106]
 }
