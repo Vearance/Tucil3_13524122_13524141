b08637df142fe3debe0ef5cfe3da90934aea8179
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-04-29 20:54:02 +0700

    refactor: main

--- src/main.go
+++ src/main.go
@@ -10,6 +10,14 @@ func main() {
 	fmt.Print("Masukan file input : ")
 	fmt.Scanln(&filename)
 
+	var algo string
+	fmt.Print("Algoritma apa yang anda pilih? (UCS/GBFS/A*)")
+	fmt.Scanln(&algo)
+
+	var heuristic string
+	fmt.Print("Heuristic apa yang anda pilih? (H1/H2/H3)")
+	fmt.Scanln(&heuristic)
+
 	board, startPos, err := LoadMap(filename)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -20,5 +28,5 @@ func main() {
 	// Solve()
 	elapsed := time.Since(start)
 
-	fmt.Printf("Waktu eksekusi: %d ms\n", elapsed.Milliseconds()) [cite: 89, 106]
+	fmt.Printf("Waktu eksekusi: %d ms\n", elapsed.Milliseconds())
 }
