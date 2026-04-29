b33f105833945016b03bcb5711e3e235e9878510
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-04-29 21:33:37 +0700

    feat: add euclidean distance

--- src/heuristic.go
+++ src/heuristic.go
@@ -43,3 +43,48 @@ func ManhattanDistance(pos Point, board *Board, target int) int {
 
 	return int(math.Abs(float64(pos.X-targetPoint.X)) + math.Abs(float64(pos.Y-targetPoint.Y)))
 }
+
+// Heuristic 2: Euclidean Distance
+// h(n) = sqrt((xi - xp)^2 + (yi - yp)^2)
+func EuclideanDistance(pos Point, board *Board, target int) int {
+    var targetPoint Point
+    found := false
+
+	// jika target masih ada di papan (kurang dari atau sama dengan angka tertinggi)
+    if target <= board.MaxTarget && board.MaxTarget != -1 {
+        targetChar := rune(target + '0')
+        for i := 0; i < board.N; i++ {
+            for j := 0; j < board.M; j++ {
+                if board.Grid[i][j] == targetChar {
+                    targetPoint = Point{i, j}
+                    found = true
+                    break
+                }
+            }
+            if found {
+                break
+            }
+        }
+    } 
+    
+    // jika semua angka sudah diambil, atau memang tidak ada angka di papan
+    if !found {
+        for i := 0; i < board.N; i++ {
+            for j := 0; j < board.M; j++ {
+                if board.Grid[i][j] == 'O' {
+                    targetPoint = Point{i, j}
+                    found = true
+                    break
+                }
+            }
+            if found {
+                break
+            }
+        }
+    }
+
+    dx := float64(pos.X - targetPoint.X)
+    dy := float64(pos.Y - targetPoint.Y)
+    
+    return int(math.Sqrt(dx*dx + dy*dy))
+}
